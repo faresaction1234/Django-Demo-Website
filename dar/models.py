e5221ca81b4cdda247c91a7a69e31c0b41daf126
ASUS <faresaction123>
2021-02-07 21:40:28 +0100

    Model is integrated

--- dar/models.py
+++ dar/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class work(models.Model):
+    work_type = models.CharField(max_length=250)
+    hours = models.IntegerField(default=0)
+    def __str__(self):
+        return self.work_type+'('+str(self.hours)+')'
+
+
--- dar/views.py
+++ dar/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import work
 # Create your views here.
 def indexpage(request):
-    return render(request, 'dar/index.html')
+    pack = work.objects.all()
+    simple_pack = list(pack)
+    context = {
+        'simple_pack': simple_pack,
+    }
+
+    return render(request, 'dar/index.html', context=context)
     #return HttpResponse('<h1>welcome to the home page</h1>')
 def about_us(request):
     nigga = [5, 7, 7]
